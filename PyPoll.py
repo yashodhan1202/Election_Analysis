67067ae8df642f2407369d5028cce42508b412ce
yashodhan1202 <yashodhan.marathe12@gmail.com>
2021-05-06 23:17:16 -0400

    Adding PyPoll.py file.

--- PyPoll.py
+++ PyPoll.py
@@ -4,8 +4,11 @@ import os
 # Assign variable for the file to load and the path
 file_to_load = os.path.join("Resources","election_results.csv")
 
-# print the path
-print(file_to_load)
+# Assign variable to save the path
+file_to_save = os.path.join("analysis","election_analysis.txt")
+
+# Initialize the total vote counter to zero
+total_votes = 0
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
@@ -15,6 +18,10 @@ with open(file_to_load) as election_data:
     
     # Print the header row
     headers = next(file_reader)
-    print(headers)
-    #for row in file_reader:
-     #   print(row[0])
+    
+    # Print each row under the header
+    for row in file_reader:
+        # Add 1 to total votes
+        total_votes += 1
+
+print(total_votes)
